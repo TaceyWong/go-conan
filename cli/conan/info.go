2910565c0f6ea2974f5cf588d66e218d9299a3f2
xinyong.w@gmail.com <xinyong.w@gmail.com>
2022-09-05 21:22:47 +0800

    update cli-info cli-install

--- cli/conan/info.go
+++ cli/conan/info.go
@@ -6,4 +6,26 @@ var InfoCMD = cli.Command{
 	Name:     "info",
 	Category: CmdCateConsumer,
 	Usage:    "Gets information about the dependency graph of a recipe.",
+	UsageText: `[-h] [--paths] [-bo BUILD_ORDER] [-g GRAPH] [-if INSTALL_FOLDER]
+               [-j [JSON]] [-n ONLY] [--package-filter [PACKAGE_FILTER]]
+               [-db [DRY_BUILD]] [-b [BUILD]] [-r REMOTE] [-u] [-l LOCKFILE]
+               [--lockfile-out LOCKFILE_OUT] [-e ENV_HOST] [-e:b ENV_BUILD]
+               [-e:h ENV_HOST] [-o OPTIONS_HOST] [-o:b OPTIONS_BUILD]
+               [-o:h OPTIONS_HOST] [-pr PROFILE_HOST] [-pr:b PROFILE_BUILD]
+               [-pr:h PROFILE_HOST] [-s SETTINGS_HOST] [-s:b SETTINGS_BUILD]
+               [-s:h SETTINGS_HOST] [-c CONF_HOST] [-c:b CONF_BUILD]
+               [-c:h CONF_HOST]
+               path_or_reference
+
+It can be used with a recipe or a reference for any existing package in
+your local cache.
+			   
+positional arguments:
+ path_or_reference     Path to a folder containing a recipe (conanfile.py or
+					   conanfile.txt) or to a recipe file. e.g.,
+					   ./my_project/conanfile.txt. It could also be a reference
+			  `,
+	Flags: []cli.Flag{
+		&cli.BoolFlag{Name: "paths", Usage: "Show package paths in local cache"},
+	},
 }
--- cli/conan/install.go
+++ cli/conan/install.go
@@ -6,6 +6,30 @@ var InstallCMD = cli.Command{
 	Name:     "install",
 	Category: CmdCateConsumer,
 	Usage:    "Installs the requirements specified in a recipe (conanfile.py or conanfile.txt).",
+	UsageText: `[-h] [-g GENERATOR] [-if INSTALL_FOLDER] [-of OUTPUT_FOLDER]
+                  [-m [MANIFESTS]] [-mi [MANIFESTS_INTERACTIVE]] [-v [VERIFY]]
+                  [--no-imports] [--build-require] [-j JSON] [-b [BUILD]]
+                  [-r REMOTE] [-u] [-l LOCKFILE] [--lockfile-out LOCKFILE_OUT]
+                  [-e ENV_HOST] [-e:b ENV_BUILD] [-e:h ENV_HOST] [-o OPTIONS_HOST]
+                  [-o:b OPTIONS_BUILD] [-o:h OPTIONS_HOST] [-pr PROFILE_HOST]
+                  [-pr:b PROFILE_BUILD] [-pr:h PROFILE_HOST] [-s SETTINGS_HOST]
+                  [-s:b SETTINGS_BUILD] [-s:h SETTINGS_HOST] [-c CONF_HOST]
+                  [-c:b CONF_BUILD] [-c:h CONF_HOST]
+                  [--lockfile-node-id LOCKFILE_NODE_ID]
+                  [--require-override REQUIRE_OVERRIDE]
+                  path_or_reference [reference]
+	
+	
+positional arguments:
+path_or_reference     Path to a folder containing a recipe (conanfile.py or
+					  conanfile.txt) or to a recipe file. e.g.,
+					  ./my_project/conanfile.txt. It could also be a reference
+reference             Reference for the conanfile path of the first argument:
+					  user/channel, version@user/channel or
+					  pkg/version@user/channel(if name or version declared in
+					  conanfile.py, they should match)
+
+`,
 	Description: `Installs the requirements specified in a recipe (conanfile.py or conanfile.txt).
 
 It can also be used to install a concrete package specifying a
@@ -16,15 +40,10 @@ to download a binary package matching the specified settings, only from
 the remote from which the recipe was retrieved. If no binary package is
 found, it can be built from sources using the '--build' option. When
 the package is installed, Conan will write the files for the specified
-generators.
-
-positional arguments:
-	path_or_reference     Path to a folder containing a recipe (conanfile.py or conanfile.txt) or to a recipe file. e.g.,
-						./my_project/conanfile.txt. It could also be a reference
-	reference             Reference for the conanfile path of the first argument: user/channel, version@user/channel or
-						pkg/version@user/channel(if name or version declared in conanfile.py, they should match)
-	`,
-	Flags: []cli.Flag{},
+generators.`,
+	Flags: []cli.Flag{
+		
+	},
 }
 
 func InstallCMDAction(c *cli.Context) (err error) {
